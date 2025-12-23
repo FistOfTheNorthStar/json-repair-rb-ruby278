53d71092710069101a2c19969c6000b21a898c53
Kaarle Kulvik <k.kulvik@gmail.com>
2025-12-23 09:50:50 +0100

    Refactor condition to check for closing braces and brackets

--- lib/json/repairer.rb
+++ lib/json/repairer.rb
@@ -54,7 +54,7 @@ module JSON
       end
 
       # repair redundant end quotes
-      while @json[@index] == CLOSING_BRACE || @json[@index] == CLOSING_BRACKET
+      while [CLOSING_BRACE, CLOSING_BRACKET].include?(@json[@index])
         @index += 1
         parse_whitespace_and_skip_comments
       end
